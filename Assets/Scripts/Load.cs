871a51600a7425a1143e41b19c7f3a6bc5366c15
Mugi <taku.13.11.29@icloud.com>
2026-01-20 23:18:14 +0900

    ローディングUI実装

---
+++ Assets/Scripts/Load.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Launcher
+{
+	/// <summary>
+	/// ロード
+	/// </summary>
+	public class Load : MonoBehaviour
+	{
+		[Header("ローディングスライダー"),SerializeField]
+		private Slider _slider;
+
+        /// <summary>
+        /// アクティブ化時の処理
+        /// </summary>
+        private void OnEnable()
+        {
+            // 総合進捗イベント登録
+            Launch.Instance.OnTotalProgress += HandleTotalProgress;
+        }
+
+        /// <summary>
+        /// 非アクティブ化時の処理
+        /// </summary>
+        private void OnDisable()
+        {
+            // 総合進捗イベント解除
+            Launch.Instance.OnTotalProgress -= HandleTotalProgress;
+        }
+
+        /// <summary>
+        /// 総合進捗ハンドラ
+        /// </summary>
+        /// <param name="progress"> 進捗（0.0～1.0） </param>
+        private void HandleTotalProgress(float progress)
+        {
+            // スライダー更新
+            // NOTE:iseki 念のためクランプ
+            _slider.value = Mathf.Clamp01(progress);
+        }
+    }
+}
